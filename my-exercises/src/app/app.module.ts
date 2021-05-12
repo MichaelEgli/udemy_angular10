af5bb03ab0a7134f8d1db4438b7f9aa6b4c722ad
MichaelEgli <michaelegli1879@gmail.com>
2021-05-12 15:58:26 +0200

    ex 3

--- my-exercises/src/app/app.module.ts
+++ my-exercises/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { DirectivesComponent } from './directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-exercises/src/app/directives/directives.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'directives',
+  templateUrl: './directives.component.html',
+  styleUrls: ['./directives.component.css']
+})
+
+export class DirectivesComponent implements OnInit {
+  doDisplay = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  saySomething() {
+    this.doDisplay = true;
+  }
+
+}
