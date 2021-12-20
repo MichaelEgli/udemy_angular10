11688522d37f14037cb0317b70f9492556c4655c
MichaelEgli <michaelegli1879@gmail.com>
2021-12-20 19:27:35 +0100

    ex reactive form - form with projectname and submit button

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      'projectData': new FormGroup( {
+        'projectname': new FormControl(null)
+      }),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
 }
