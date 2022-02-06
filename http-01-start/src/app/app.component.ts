801419978f618528dbe61bdfae567596c4c91dc4
MichaelEgli <michaelegli1879@gmail.com>
2022-02-06 20:27:56 +0100

    ex258

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -37,6 +38,15 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http.get('https://ng-complete-guide-53bf4-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+    .pipe(map(responseData => {
+      const postsArray = [];
+      for (const key in responseData) {
+        if (responseData.hasOwnProperty(key)) {
+          postsArray.push({ ...responseData[key], id: key});
+        }
+      }
+      return postsArray;
+    }))
     .subscribe(posts => {
       console.log(posts);
     });
