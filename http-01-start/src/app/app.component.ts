0c6e7b2cd13035b7bb282fda41831a3abe98835c
MichaelEgli <michaelegli1879@gmail.com>
2022-02-22 08:24:08 +0100

    ex268

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -29,6 +29,7 @@ export class AppComponent implements OnInit, OnDestroy {
       this.isFetching = false;
       this.loadedPosts = posts;
     }, error => {
+      this.isFetching = false;
       this.error = error.message;
       console.log(error);
     });
@@ -45,6 +46,10 @@ export class AppComponent implements OnInit, OnDestroy {
     this.postsService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, error => {
+      this.isFetching = false;
+      this.error = error.message;
+      console.log(error);
     });
   }
 
@@ -53,6 +58,10 @@ export class AppComponent implements OnInit, OnDestroy {
     this.postsService.deletePost().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  onHandleError() {
+    this.error = null;
   }
 
   ngOnDestroy(): void {
