ef18fad6ad1ac70ced63e28b2df89867121c6c70
MichaelEgli <michaelegli1879@gmail.com>
2022-03-13 20:05:05 +0100

    ex276

--- http-01-start/src/app/auth-interceptor.service.ts
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -3,16 +3,8 @@ import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        console.log('Requst is on its way');
-        console.log(req.url);
         const modifiedRequst = req.clone({headers: req.headers.append('Auth', 'xyz')
         });
-        return next.handle(modifiedRequst).pipe(tap(event => {
-            console.log(event);
-            if (event.type === HttpEventType.Response) {
-                console.log('Response arrived, body data: ');
-                console.log(event.body);
-            }
-        }));
+        return next.handle(modifiedRequst);
     }
 }
