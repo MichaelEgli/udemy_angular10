7ad0ac96f801a7b1e3253cf4e5739e5512a61eed
MichaelEgli <michaelegli1879@gmail.com>
2022-03-13 19:23:20 +0100

    ex171

--- http-01-start/src/app/post.service.ts
+++ http-01-start/src/app/post.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
-import { map, catchError } from "rxjs/operators"; 
+import { map, catchError, tap } from "rxjs/operators"; 
 import { Post } from "./post.model";
 
 @Injectable({providedIn: 'root'})
@@ -16,9 +16,12 @@ export class PostsService {
         this.http
         .post< { name: string }>(
           'https://ng-complete-guide-53bf4-default-rtdb.europe-west1.firebasedatabase.app/posts.json', 
-        postData
+        postData,
+        {
+          observe: 'response'
+        }
         ).subscribe(responseData => {
-          console.log(responseData);
+          console.log(responseData.body);
         }, error => {
           this.error.next(error.message);
         });
@@ -53,6 +56,20 @@ export class PostsService {
     }
 
     deletePost() {
-      return this.http.delete('https://ng-complete-guide-53bf4-default-rtdb.europe-west1.firebasedatabase.app/posts.json');
+      return this.http.delete(
+        'https://ng-complete-guide-53bf4-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+        {
+          observe: 'events'
+        }
+      ).pipe(
+        tap(event => {
+        console.log(event);
+        if (event.type === HttpEventType.Sent) {
+          // .... 
+        }
+        if (event.type === HttpEventType.Response) {
+          console.log(event.body);
+        }
+      }));
     }
 }
